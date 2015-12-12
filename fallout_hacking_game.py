e6c734a1a0e352dd6007b6d9d706b4fe13b086fe
Linkus000 <juliusz.kk@gmail.com>
2015-12-12 16:19:02 +0100

    Balancing hardest difficulty

--- fallout_hacking_game.py
+++ fallout_hacking_game.py
@@ -4,7 +4,6 @@ from random import randint, sample
 fp=open("enable1.txt", "r")
 linesv2=[]
 correct=0
-guesses=4
 lines=fp.read().upper()
 lines=lines.split()
 l=randint(4, 15)
@@ -12,8 +11,12 @@ for i in range(len(lines)):
     if len(lines[i])==int(l):
         linesv2.append(lines[i])
 linesv2 = sample(linesv2, 4*int(input("Difficulty (1-5)? ")))
+if len(linesv2)==20:
+    [linesv2.pop() for i in range(5)]
+    guesses=3
+else: guesses=4
 [print(i) for i in linesv2]
-linesv2=''.join(sample(linesv2, 1)).rstrip()
+linesv2=''.join(sample(linesv2, 1))
 while True:
     word = input("Guess (%i left)? " % guesses)
     guesses-=1
