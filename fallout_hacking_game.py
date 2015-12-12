44db99dc4e5dfb2c0e567b6e9f7033aeec42cbef
Linkus000 <juliusz.kk@gmail.com>
2015-12-12 16:10:27 +0100

    Lowering the amount of code

--- fallout_hacking_game.py
+++ fallout_hacking_game.py
@@ -2,18 +2,17 @@
 
 from random import randint, sample
 fp=open("enable1.txt", "r")
-lines=[]
 linesv2=[]
 correct=0
 guesses=4
-for line in fp:
-    lines.append(line.upper())
+lines=fp.read().upper()
+lines=lines.split()
 l=randint(4, 15)
 for i in range(len(lines)):
     if len(lines[i])==int(l):
         linesv2.append(lines[i])
 linesv2 = sample(linesv2, 4*int(input("Difficulty (1-5)? ")))
-[print(i, end='') for i in linesv2]
+[print(i) for i in linesv2]
 linesv2=''.join(sample(linesv2, 1)).rstrip()
 while True:
     word = input("Guess (%i left)? " % guesses)
