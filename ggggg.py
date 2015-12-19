10713d74b3a967a5693ab8a94941c07c8bb5d9b1
Linkus000 <juliusz.kk@gmail.com>
2015-12-19 12:34:23 +0100

    Challenge #245

---
+++ ggggg.py
@@ -0,0 +1,20 @@
+code=input().split()
+string=input()
+chars=[]
+ggg=[]
+decoded=''
+buffer=''
+for i in range(0, len(code), 2):
+    chars.append(code[i])
+for i in range(1, len(code), 2):
+    ggg.append(code[i])
+for i in range(len(string)):
+    if string[i] not in 'gG':
+        decoded+=string[i]
+    else:
+        buffer+=string[i]
+        for x in range(len(ggg)):
+            if buffer==ggg[x]:
+                decoded+=chars[x]
+                buffer=''
+print(decoded)
